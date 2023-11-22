9fcd7c3d7070a8c23f17a24e3c289ec5c61273d5
EwngIsNewbie <2408466294@qq.com>
2023-11-22 20:11:34 +0800

    新增TeamUserVO，返回前端

---
+++ src/main/java/cn/zhuhai/usercenter/model/vo/TeamUserVO.java
@@ -0,0 +1,75 @@
+package cn.zhuhai.usercenter.model.vo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Author Ewng
+ * @Description 用户队伍信息类封装（脱敏）
+ * @Date 2023/11/22 15:14
+ */
+@Data
+public class TeamUserVO implements Serializable {
+
+    private static final long serialVersionUID = -5569842581463587320L;
+    /**
+     *
+     */
+
+    private Long id;
+
+    /**
+     * 队伍名称
+     */
+    private String name;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 最大人数
+     */
+    private Integer maxNum;
+
+    /**
+     * 过期时间
+     */
+    private Date expireTime;
+
+    /**
+     * 用户Id
+     */
+    private Long userId;
+
+    /**
+     * 状态 0-公开 1-私有 2-加密
+     */
+    private Integer status;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 创建人用户
+     */
+    private UserVO userVO;
+}
